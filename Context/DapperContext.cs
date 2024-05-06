0d7bb73e8878602e28860df8dd9be5279efad240
vineeshvs4151 <vineeshvs4151@gmail.com>
2024-05-06 10:59:58 +0530

    Add project files.

---
+++ Context/DapperContext.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Digista_Web_Api.Context
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionstring;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionstring = _configuration.GetConnectionString("MySqlConnection");
+        }
+
+        public IDbConnection CreateConnection() => new MySqlConnection(_connectionstring);
+    }
+}
